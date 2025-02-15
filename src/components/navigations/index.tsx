73046655b40639233b88fb3bb148288c9015c38c
murukal <tutu@fantufantu.com>
2025-02-15 10:29:38 +0800

    update dependencies to latest versions; enhance navigation component with icon buttons and modify button shape in home page

--- src/components/navigations/index.tsx
+++ src/components/navigations/index.tsx
@@ -1,9 +1,29 @@
 import Logo from "./logo";
+import { AddAlert } from "musae/icons";
+import { Space, IconButton } from "musae";
 
 const Navigations = () => {
   return (
     <aside className="p-8">
       <Logo />
+
+      <Space gutter={48} orientation="vertical" className="mt-20">
+        <IconButton>
+          <AddAlert size={28} />
+        </IconButton>
+
+        <IconButton variant="text">
+          <AddAlert size={28} />
+        </IconButton>
+
+        <IconButton variant="text">
+          <AddAlert size={28} />
+        </IconButton>
+
+        <IconButton variant="text">
+          <AddAlert size={28} />
+        </IconButton>
+      </Space>
     </aside>
   );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,7 +10,9 @@ const Home = () => {
       <div className="px-9 py-8 rounded-[20px] bg-color-on-primary">
         <h1 className="text-3xl">Today Task</h1>
         <p className="mt-2">Check your daily tasks and schedules</p>
-        <Button className="mt-4">Today’s schedule</Button>
+        <Button className="mt-4" shape="rectangular">
+          Today’s schedule
+        </Button>
       </div>
     </div>
   );
