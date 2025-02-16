6681915e417cd2b59d5328609db9e140aad6c1fd
murukal <tutu@fantufantu.com>
2025-02-16 23:48:54 +0800

    refactor layout component; rename tauri script and integrate Bench component with navigation structure

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,22 +1,40 @@
-import { useTheme } from "musae";
+import { useTheme, Bench } from "musae";
 import Navigations from "../components/navigations";
 import { type ReactNode } from "react";
+import Logo from "../components/navigations/logo";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   const { colors } = useTheme();
 
   return (
-    <div
-      className="w-screen h-screen flex"
+    <Bench
       style={{
         // @ts-expect-error style vars
         "--color-surface-container-low": colors["surface-container-low"],
         "--color-on-primary": colors["on-primary"],
       }}
+      title="Cabin-Cab"
+      layout="side"
+      navigations={[
+        {
+          label: "测试",
+          path: "/task",
+          children: [
+            {
+              label: "child1",
+              path: "/task/child1",
+            },
+          ],
+        },
+        {
+          label: "测试2",
+          path: "/task2",
+        },
+      ]}
+      logo={<Logo />}
     >
-      <Navigations />
-      <main className="flex-1 py-10 px-8 bg-color-surface-container-low">{children}</main>
-    </div>
+      {children}
+    </Bench>
   );
 };
 
