144aea1a406a8f197b03d3f408a8495ae54676d5
murukal <tutu@fantufantu.com>
2025-02-27 22:36:21 +0800

    update musae to version 1.0.2; enhance disk info handling and display usage percentage in Home component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,20 @@
 import { Input, Button, Progress } from "musae";
-import { useEffect, useState } from "react";
-import { invoke } from "@tauri-apps/api/core";
+import { useEffect, useMemo, useState } from "react";
 import { getDiskInfo, type DiskInfo } from "../../api/disk";
 
 const Home = () => {
   const [diskInfo, setDiskInfo] = useState<DiskInfo>({ total: 0, used: 0 });
 
   useEffect(() => {
-    getDiskInfo().then(setDiskInfo);
+    getDiskInfo()
+      .then(setDiskInfo)
+      .catch(() => null);
   }, []);
 
-  console.log("setDiskInfo===", diskInfo);
+  const usedDisk = useMemo(() => {
+    if (!diskInfo.total) return 0;
+    return Math.round((diskInfo.used / diskInfo.total) * 100);
+  }, [diskInfo]);
 
   return (
     <div className="flex flex-col gap-8">
@@ -26,7 +30,10 @@ const Home = () => {
         </Button>
       </div>
 
-      <Progress variant="circular" value={50} />
+      <div>
+        硬盘占用
+        <Progress variant="circular" value={usedDisk} />
+      </div>
     </div>
   );
 };
